ff36675c2b738f640722565f27291a9246b5b8ef
Matthew Wyatt <matthewwyatt@pop-os.localdomain>
2023-01-11 01:11:48 -0600

    prototype

--- controller.py
+++ controller.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, send_file
 import psycopg2
 
 app = Flask(__name__)
 
 @app.route("/")
 def opening_page_hmtl():
-    html = open("HTML/opening_page.html")
+    html = open("HTML/opening-page.html")
     return html.read()
+
+@app.route("/CSS/style.css")
+def getStyle():
+    return send_file("CSS/style.css", mimetype="text/css")
+
+@app.route("/Images/cat.png")
+def getImage():
+    return send_file("Images/cat.png", mimetype="image/png")
 
 @app.route("/getNotes")
 def getNotes():
@@ -20,7 +28,7 @@ def getNotes():
     cursor.execute("select * from test")
     return cursor.fetchone()[0]
 
-@app.route("/Javascript/opening_page.js")
+@app.route("/Javascript/opening-page.js")
 def opening_page_js():
-    js = open("Javascript/opening_page.js")
+    js = open("Javascript/opening-page.js")
     return js.read()
