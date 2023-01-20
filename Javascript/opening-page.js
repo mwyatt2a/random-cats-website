213185930032a7b73825e9d2dde60271bcffcf6a
Matthew Wyatt <matthewwyatt@pop-os.localdomain>
2023-01-19 22:25:16 -0600

    useable

--- Javascript/opening-page.js
+++ Javascript/opening-page.js
@@ -2,6 +2,20 @@ function change() {
     fetch("/changeImage").then(data => data.text()).then(result => {
         document.getElementById("cat").src = result;
         document.getElementById("source").href = result;
-        });
+    });
 }
 change();
+function add() {
+    let url = prompt("Enter the url of the new image");
+    fetch("/add?url=" + url).then((response) => response.text()).then((text) => {
+        if (text === "success") {
+            alert("Image Added Successfully!");
+        }
+        else if (text === "duplicate") {
+            alert("That URL Was Already Added");
+        }
+        else {
+            alert("Something Went Wrong :( " + text);
+        }
+    });
+}
--- controller.py
+++ controller.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, request
 import psycopg2
 import random
 
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def opening_page_hmtl():
     return send_file("HTML/opening-page.html", mimetype="text/html")
 
-@app.route("/Images/favicon.ico")
+@app.route("/images/favicon.ico")
 def getFavicon():
     return send_file("Images/favicon.ico", mimetype="image/x-icon")
 
@@ -24,8 +24,21 @@ def newImage():
     count = cursor.fetchone()[0]
     randid = random.randrange(1,count+1)
     cursor.execute("select * from urls where id = {};".format(randid))
-    return cursor.fetchone()[1]
-
+    url = cursor.fetchone()[1]
+    connection.close()
+    return url
 @app.route("/Javascript/opening-page.js")
 def opening_page_js():
     return send_file("Javascript/opening-page.js", mimetype="text/javascript")
+
+@app.route("/add", methods=["GET"])
+def addURL():
+    params = request.args
+    url = params.get("url")
+    connection = psycopg2.connect(user="root", password="root", host="localhost", port="5432", dbname="flaskdata")
+    cursor = connection.cursor()
+    cursor.execute("insert into urls (url, reports, views) values ('{}', 0, 0);".format(url))
+    connection.commit()
+    connection.close()
+    return "success"
+   
