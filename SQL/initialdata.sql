4e717bd4381fc2d74e7f101f8a98096cc5ff4dd4
Matthew Wyatt <matthewwyatt@pop-os.localdomain>
2023-01-21 17:15:58 -0600

    initial website done

---
+++ SQL/initialdata.sql
@@ -0,0 +1,5 @@
+insert into urls (url, views, reports) values ('https://cache.desktopnexus.com/thumbseg/2357/2357493-bigthumbnail.jpg', 0, 0);
+insert into urls (url, views, reports) values ('https://preview.redd.it/f6v3xpg47ii31.jpg?auto=webp&s=f96790cd87ff619d2d94646a8e883365f6d202d9', 0, 0);
+insert into urls (url, views, reports) values ('https://www.rd.com/wp-content/uploads/2021/04/GettyImages-1271172807.jpg', 0, 0);
+insert into urls (url, views, reports) values ('https://i.pinimg.com/originals/de/9f/18/de9f180a62deadfcd8197dc66b52205e.jpg', 0, 0);
+insert into urls (url, views, reports) values ('https://www.pngall.com/wp-content/uploads/2016/03/Cat-PNG-2.png', 0, 0);
