44e6c3698bca2da9d21fc0164756a652f2938192
Matthew Wyatt <matthewwyatt@pop-os.localdomain>
2023-02-10 06:47:38 -0600

    improvements

--- javascript/webgl.js
+++ javascript/webgl.js
@@ -19,16 +19,78 @@ function matrixMultiply(A, B) {
     C[15] = A[3]*B[12] + A[7]*B[13] + A[11]*B[14] + A[15]*B[15];
     return C;
 }
-function createTransformationMatrix(scale, ztheta, ytheta, xtheta, xtrans, ytrans, ztrans, aspect, fieldOfView, near, far) {
+function matrixInverse(A) {
+    let f = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    f[0] = A[10]*A[15] - A[11]*A[14];
+    f[1] = A[9]*A[15] - A[11]*A[13];
+    f[2] = A[9]*A[14] - A[10]*A[13];
+    f[3] = A[6]*A[16] - A[7]*A[14];
+    f[4] = A[5]*A[16] - A[7]*A[13];
+    f[5] = A[5]*A[14] - A[6]*A[13];
+    f[6] = A[6]*A[11] - A[7]*A[10];
+    f[7] = A[5]*A[11] - A[7]*A[9];
+    f[8] = A[5]*A[10] - A[6]*A[9];
+    f[9] = A[8]*A[15] - A[11]*A[12];
+    f[10] = A[8]*A[14] - A[10]*A[12];
+    f[11] = A[4]*A[15] - A[7]*A[12];
+    f[12] = A[4]*A[14] - A[6]*A[12];
+    f[13] = A[4]*A[11] - A[11]*A[8];
+    f[14] = A[4]*A[10] - A[6]*A[8];
+    f[15] = A[8]*A[13] - A[9]*A[12];
+    f[16] = A[4]*A[13] - A[5]*A[12];
+    f[17] = A[4]*A[9] - A[5]*A[8];
+    let C = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    C[0] = A[5]*f[0] - A[6]*f[1] + A[7]*f[2];
+    C[1] = -A[1]*f[0] + A[2]*f[1] - A[3]*f[2];
+    C[2] = A[1]*f[3] - A[2]*f[4] + A[3]*f[5];
+    C[3] = -A[1]*f[6] + A[2]*f[7] - A[3]*f[8];
+    C[4] = -A[4]*f[0] + A[6]*f[9] - A[7]*f[10];
+    C[5] = A[0]*f[0] - A[2]*f[9] + A[3]*f[10];
+    C[6] = -A[0]*f[3] + A[2]*f[11] - A[3]*f[12];
+    C[7] = A[0]*f[6] - A[2]*f[13] + A[3]*f[14];
+    C[8] = A[4]*f[1] - A[5]*f[9] + A[7]*f[15];
+    C[9] = -A[0]*f[1] + A[1]*f[9] - A[3]*f[15];
+    C[10] = A[0]*f[4] - A[1]*f[11] + A[3]*f[16];
+    C[11] = -A[0]*f[7] + A[1]*f[13] - A[3]*f[17];
+    C[12] = -A[4]*f[2] + A[5]*f[10] - A[6]*f[15];
+    C[13] = A[0]*f[2] - A[1]*f[10] + A[2]*f[15];
+    C[14] = -A[0]*f[5] + A[1]*f[12] - A[2]*f[16];
+    C[15] = A[0]*f[8] - A[1]*f[14] + A[2]*f[17];
+    let determinant = A[0]*C[0] + A[1]*C[4] + A[2]*C[8] + A[3]*C[12];
+    return matrixMultiply([1/determinant, 0, 0, 0, 0, 1/determinant, 0, 0, 0, 0, 1/determinant, 0, 0, 0, 0, 1], C);
+}
+function vectorNormalize(v) {
+    let magnitude = Math.sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]); 
+    return [v[0]/magnitude, v[1]/magnitude, v[2]/magnitude];
+}
+function vectorCross(v1, v2) {
+    return [v1[1]*v2[2] - v1[2]*v2[1], v1[2]*v2[0] - v1[0]*v2[2], v1[0]*v2[1] - v1[1]*v2[0]];
+}
+function createTransformationMatrix(scale, ztheta, ytheta, xtheta, xtrans, ytrans, ztrans, aspect, fieldOfView, near, far, focus, camztheta, camytheta, camxtheta, camx, camy, camz, focusx, focusy, focusz) {
     let scaling = [scale, 0, 0, 0, 0, scale, 0, 0, 0, 0, scale, 0, 0, 0, 0, 1];
     let zRotation = [Math.cos(ztheta), Math.sin(ztheta), 0, 0, -Math.sin(ztheta), Math.cos(ztheta), 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
     let yRotation = [Math.cos(ytheta), 0, -Math.sin(ytheta), 0, 0, 1, 0, 0, Math.sin(ytheta), 0, Math.cos(ytheta), 0, 0, 0, 0, 1];
     let xRotation = [1, 0, 0, 0, 0, Math.cos(xtheta), Math.sin(xtheta), 0, 0, -Math.sin(xtheta), Math.cos(xtheta), 0, 0, 0, 0, 1];
     let translation = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, xtrans, ytrans, ztrans, 1];
+    let cameraMatrix = [];
+    if (!focus) {
+        let camzRotation = [Math.cos(camztheta), Math.sin(camztheta), 0, 0, -Math.sin(camztheta), Math.cos(camztheta), 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
+        let camyRotation = [Math.cos(camytheta), 0, -Math.sin(camytheta), 0, 0, 1, 0, 0, Math.sin(camytheta), 0, Math.cos(camytheta), 0, 0, 0, 0, 1];
+        let camxRotation = [1, 0, 0, 0, 0, Math.cos(camxtheta), Math.sin(camxtheta), 0, 0, -Math.sin(camxtheta), Math.cos(camxtheta), 0, 0, 0, 0, 1];
+        let camtranslation = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, camx, camy, camz, 1];
+        cameraMatrix = matrixMultiply(camtranslation, matrixMultiply(camxRotation, matrixMultiply(camyRotation, matrixMultiply(camzRotation, [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]))));
+    }
+    else {
+        let newZ = vectorNormalize([camx - focusx, camy - focusy, camz - focusz]);
+        let newX = vectorNormalize(vectorCross([0, 1, 0], newZ));
+        let newY = vectorNormalize(vectorCross(newZ, newX));
+        cameraMatrix = [newX[0], newZ[1], newZ[2], 0, newY[0], newY[1], newY[2], 0, newZ[0], newZ[1], newZ[2], 0, camx, camy, camz, 1];
+    }
+    let viewMatrix = cameraMatrix;//matrixInverse(cameraMatrix);
     let f = Math.tan(Math.PI*0.5 -0.5*fieldOfView);
     let rangeInv = 1.0/(near - far);
     let projection = [f/aspect, 0, 0, 0, 0, f, 0, 0, 0, 0, (near + far)*rangeInv, -1, 0, 0, near*far*rangeInv*2, 0];
-    return matrixMultiply(projection, matrixMultiply(translation, matrixMultiply(xRotation, matrixMultiply(yRotation, matrixMultiply(zRotation, scaling)))));
+    return matrixMultiply(projection, matrixMultiply(viewMatrix, matrixMultiply(translation, matrixMultiply(xRotation, matrixMultiply(yRotation, matrixMultiply(zRotation, scaling))))));
 }
 const canvas = document.querySelector("#webgl");
 const gl = canvas.getContext("webgl2");
@@ -239,6 +301,11 @@ function render() {
     if (loop >= 30) {
         loop = 0;
         times *= -1;
+        cameraLoop++;
+    }
+    if (cameraLoop >= 10) {
+        cameraLoop = 0;
+//        focus = !focus;
     }
     loop++;
     xtrans += times*10;
@@ -266,7 +333,7 @@ function render() {
     gl.bindFramebuffer(gl.FRAMEBUFFER, fbo2);
     gl.uniform1fv(kernelLocation, emboss);
     gl.drawArrays(primitiveType, offset, count);
-    let transformationMatrix = createTransformationMatrix(scale, ztheta, ytheta, xtheta, xtrans, ytrans, ztrans, aspect, Math.PI/3, 10, 2000);
+    let transformationMatrix = createTransformationMatrix(scale, ztheta, ytheta, xtheta, xtrans, ytrans, ztrans, aspect, Math.PI/3, 10, 2000, focus, camztheta, camytheta, camxtheta, camx, camy, camz, focusx, focusy, focusz);
     gl.uniformMatrix4fv(transformationLocation, false, transformationMatrix);
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
     gl.bindTexture(gl.TEXTURE_2D, backTexture2);
@@ -295,5 +362,16 @@ let ytheta = -Math.PI/2;
 let xtheta = -Math.PI/2;
 let scale = 1.7;
 let loop = 0;
+let cameraLoop = 0;
+let focus = false;
+let camztheta = 0;
+let camytheta = 0;
+let camxtheta = 0;
+let camx = 0;
+let camy = 0;
+let camz = 0;
+let focusx = -1;
+let focusy = -1;
+let focusz = -1;
 let times = 1;
 setInterval(() => render(), 50);
