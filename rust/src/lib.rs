de822615d5186df43ec35a1cb209eab8020e6cd5
Matthew Wyatt <matthewwyatt@pop-os.localdomain>
2023-04-06 14:54:59 -0500

    wasm tests

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -70,7 +70,7 @@ impl GraphicsMatrix {
 
     fn multiply(&self, b: &Self) -> Self {
         let mut c = Self {
-            data: [1000.0; 16],
+            data: [0.0; 16],
         };
         for columns in 0..4 {
             for rows in 0..4 {
@@ -151,7 +151,7 @@ impl GraphicsMatrix {
         let x_rotation = Self {
             data: [1.0, 0.0, 0.0, 0.0, 0.0, thetas.2.cos(), thetas.2.sin(), 0.0, 0.0, -thetas.2.sin(), thetas.2.cos(), 0.0, 0.0, 0.0, 0.0, 1.0],
         };
-        x_rotation.multiply(&y_rotation).multiply(&z_rotation)
+        x_rotation.multiply(&x_rotation)//.multiply(&y_rotation).multiply(&z_rotation)
     }
 
     pub fn create_translation_matrix(trans: &Translation) -> Self {
